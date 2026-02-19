cf2c6816d03282dc00d58d2141d618d7dd9218fa
daus <alfi.j234@gmail.com>
2026-02-19 14:06:41 +0700

    komenin root di vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  root: "src",
+  // root: "./src", // unkomen buat jalanin di lokal, komenin buat jalanin di github
   // This tells Vite to use the Tailwind v4 plugin
   plugins: [tailwindcss()],
 });
