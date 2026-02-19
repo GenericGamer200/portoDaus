08d5a9749f01aee524765a99f68ebe5232dc4b01
daus <alfi.j234@gmail.com>
2026-02-19 14:26:20 +0700

    apus vite config di src

--- src/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  root: "src",
-  // This tells Vite to use the Tailwind v4 plugin
-  plugins: [tailwindcss()],
-});
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // root: "./src", // unkomen buat jalanin di lokal, komenin buat jalanin di github
   // This tells Vite to use the Tailwind v4 plugin
   plugins: [tailwindcss()],
 });
